c5017e0d3ada28fedb5788f38b746d4fc4e5c5e9
Rachel Roberts <rachel.roberts@digital.cabinet-office.gov.uk>
2025-01-28 11:31:00 +0000

    LIME-1512 updated common express with cookie banner fix

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require("express");
 require("express-async-errors");
-const { setup } = require("hmpo-app");
+const { setup } =
+  require("@govuk-one-login/di-ipv-cri-common-express").bootstrap;
 const addLanguageParam = require("@govuk-one-login/frontend-language-toggle/build/cjs/language-param-setter.cjs");
 
 const RoutingService = require("./router");
