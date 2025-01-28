76fe73608bc66d5e836dbb418926c2494b8fa479
Seamus McShane <seamus.mcshane@digital.cabinet-office.gov.uk>
2025-01-28 15:20:18 +0000

    LIME-1352 Add SIGTERM shutdown, and adjust keepalivetimeout duration to longer than loadbalancer timeout (DCP-4450)

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 require("express");
 require("express-async-errors");
+const { PORT } = require("./lib/config");
 const { setup } = require("hmpo-app");
 const addLanguageParam = require("@govuk-one-login/frontend-language-toggle/build/cjs/language-param-setter.cjs");
 
@@ -18,3 +19,31 @@ app.get("nunjucks").addGlobal("addLanguageParam", addLanguageParam);
 
 AppSetup.init(app, router);
 RoutingService.init(router);
+
+/* Server configuration */
+const server = app.listen(PORT);
+
+// AWS recommends the keep-alive duration of the target is longer than the idle timeout value of the load balancer (default 60s)
+// to prevent possible 502 errors where the target connection has already been closed
+// https://docs.aws.amazon.com/elast
+server.keepAliveTimeout = 65000;
+
+// Handles graceful shutdown of the NODE service, so that if the container is killed by a SIGTERM, it finishes processing existing connections before the server shuts down.
+process.on("SIGTERM", () => {
+  // eslint-disable-next-line no-console
+  console.log("SIGTERM signal received: closing HTTP server");
+
+  server.close((err) => {
+    if (err) {
+      // eslint-disable-next-line no-console
+      console.log(
+        `Error while calling server.close() occurred: ${err.message}`
+      );
+      process.exit(-1);
+    } else {
+      // eslint-disable-next-line no-console
+      console.log("HTTP server closed");
+      process.exit(0);
+    }
+  });
+});
