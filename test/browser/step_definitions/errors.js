5f1cad4b30707d873ba3e61245c5e19ca280cf74
Chris Bates <chris.bates@digital.cabinet-office.gov.uk>
2025-04-10 17:12:25 +0100

    LIME-1637
    Updated Axe Accessibility Tests:
    
    Removed the Axe Scan from the background step of the Scenarios and within Error Page Step
    Added Specific Test for Passport Details Page and Passport Error Page which runs the Axe Scan
    
    This will allow for clearer reporting and troubleshooting of any accessibility issues going forward

--- test/browser/step_definitions/errors.js
+++ test/browser/step_definitions/errors.js
@@ -8,19 +8,22 @@ const { injectAxe } = require("axe-playwright");
 
 Then("they should see an error page", async function () {
   const errorPage = new ErrorPage(this.page);
-
   const errorTitle = await errorPage.getErrorTitle();
-
-  await injectAxe(this.page);
-  // Run Axe for WCAG 2.2 AA rules
-  const wcagResults = await this.page.evaluate(() => {
-    return axe.run({
-      runOnly: ["wcag2aa"]
-    });
-  });
-  expect(wcagResults.violations, "WCAG 2.2 AAA violations found").to.be.empty;
-
   expect(errorTitle.trim()).to.equal(
     errorPage.getSomethingWentWrongMessage().trim()
   );
 });
+
+Then(
+  "I run the Axe Accessibility check against the Error entry page",
+  async function () {
+    await injectAxe(this.page);
+    // Run Axe for WCAG 2.2 AA rules
+    const wcagResults = await this.page.evaluate(() => {
+      return axe.run({
+        runOnly: ["wcag21aa"]
+      });
+    });
+    expect(wcagResults.violations).to.be.empty;
+  }
+);
